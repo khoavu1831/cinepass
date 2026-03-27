6a1e18dfbfb89d323340711f5cb596f6acafd12a
kaiwu <khoavu1831@gmail.com>
2026-03-28 01:32:32 +0700

    feat: add filter

--- src/pages/Search/components/Main.jsx
+++ src/pages/Search/components/Main.jsx
@@ -9,13 +9,14 @@ function Main() {
     <div className="search-container py-28 h-full bg-[#1b1d29]">
       <div className="content px-5">
         {/* header */}
-        <div className="rows-header text-3xl font-medium text-white flex items-center gap-2">
-          <i class="fa-solid fa-sliders"></i>
+        <div className="rows-header text-2xl font-medium text-white flex items-center gap-2">
+          <i className="fa-solid fa-sliders"></i>
           <h1>Kết quả tìm kiếm "phim"</h1>
         </div>
 
         {/* main content */}
         <div className="main-content">
+          {/* type filter: movie / cast */}
           <div className="toggle-btns flex gap-2 py-5">
             <Button
               active={type === "phim"}
@@ -29,9 +30,13 @@ function Main() {
             />
           </div>
 
+          {/* advanced filter */}
           <div className="filter">
             <Fieldset />
           </div>
+
+          {/* result movies */}
+          
         </div>
       </div>
     </div>
